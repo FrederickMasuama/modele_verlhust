407ed3b254d31cf898451242041e66c81ed4600b
FrédérickMAS <frederickmasuama@gmail.com>
2024-11-28 09:19:20 +0100

    did I get it?

--- pop_verlhust.py
+++ pop_verlhust.py
@@ -1,9 +1,5 @@
 """Etude de la population via Verlhust"""
-<<<<<<< HEAD
-print("hello world")
-=======
 a= 'alpha'
 b = 'romeo'
-print("hellobbb")
+print("hello", b)
 
->>>>>>> 741f3e972c3cbc6498ad8e270625f11c8e732772
