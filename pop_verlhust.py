f47f444e1562b40ef452f11be2b47e7dce6e43e0
FrédérickMAS <frederickmasuama@gmail.com>
2024-12-05 21:49:16 +0100

    ajout des affichages et correction d'erreurs

--- pop_verlhust.py
+++ pop_verlhust.py
@@ -22,7 +22,7 @@ else :
     a = int(input("Veillez rentrer le taux de natalité"))
     b = int(input("Veuillez rentrer le taux de mortalité"))
     r = a - b
-    t = np.array(0, 100, 100)
+    t = np.linspace(0, 100, 100)
     p = int(input("Veuillez taper 1 si vous avez la valeur numérique de la capacité biotique \n ou 2 si vous n'avez que le coefficient de la compétition"))
     if p == 1 :
         k = int(input("Veuillez rentrer la valeur numérique de la capacité biotique"))
@@ -31,8 +31,15 @@ else :
         while c == 0 :
             c = int(input("Veuillez rentrer une valeur autre que zéro s'il vous plaît"))
         k = r/c
-
+        
     z = f(r, k, N_0, t)
+
+#affichages
+
+    plt.plot(t,z, label = "Courbe d'évolution")
+    plt.hist(t)
+    plt.legend()
+    plt.show()
 
     #points d'équilibres
     #points de stabilité
